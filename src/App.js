118b57163f98e903dcd85485d64ef8300f15fec0
CesarMoschini <cesar_moschini@hotmail.com>
2022-07-25 15:38:35 -0300

    Desafío de Contador

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Counter from './components/Counter/Counter';
 
 function App() {
+  const handleOnAdd = (cantidad) => {
+  console.log(`La cantidad seleccionada es: ${cantidad}`)
+  }
   return (
     <div className="App">
       <Navbar/>
       <ItemListContainer saludo='Bienvenido al seleccionador de cápsulas'/>
+      <Counter inventario = {22} onAdd={handleOnAdd}/>
     </div>
   );
+
 }
 
 export default App;
